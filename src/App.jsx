1547a2f3d1a049d82568af08e530d82747d0e01d
Alan <ftcursosap@gmail.com>
2026-03-06 17:55:05 -0600

    Semana 10 - Dia 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <h1>Mi primer React de Alan</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Hola, estas en React</h1>
+      <p>Este es mi primer JSX en React.</p>
+      <p>Mi nombre es Alan</p>
+      <button>Presiona este boton</button>
+      <ul>
+        <li>Alma</li>
+        <li>Irma</li>
+        <li>Itzel</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App
