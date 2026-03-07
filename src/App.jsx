e0018bb110e3bf66b9ca6802a447f8a698201ed6
Alan <ftcursosap@gmail.com>
2026-03-07 14:04:05 -0600

    Semana 10 - Dia 5

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,17 @@ import './App.css'
 function App() {
 
   const nombre = "Alan";
-  var edad = 29;
-  const colores = ["Naranja", "Verde", "Rojo", "Azul"];
-  return (
+  
+  const frutas = ["Manzana", "Pera", "Mango", "Platano"];
+
+  return(
     <div>
-      <h1>Hola {nombre}, estas en React</h1>
-      <h2>{nombre}</h2>
-      <p>Este es mi primer JSX en React.</p>
-      <p>Mi nombre es Alan</p>
-      <p>Mi edad en 5 años sera: {edad + 5}</p>
-      <button>Presiona este boton</button>
+      <h1>Lista de frutas</h1>
+
       <ul>
-        <li>{colores[0]}</li>
-        <li>{colores[1]}</li>
-        <li>{colores[2]}</li>
-        <li>{colores[3]}</li>
-      </ul>
-      <ul>
-        <li>Alma</li>
-        <li>Irma</li>
-        <li>Itzel</li>
+        {frutas.map(function(fruta, index) {
+          return <li key={index}>{fruta}</li>;
+        })}
       </ul>
     </div>
   );
