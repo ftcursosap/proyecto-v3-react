cf3aa4b8469d81a55a457817c19525fe69de165f
Alan <ftcursosap@gmail.com>
2026-03-06 18:01:00 -0600

    Semana 10 - Dia 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  const nombre = "Alan";
+  var edad = 29;
+  const colores = ["Naranja", "Verde", "Rojo", "Azul"];
   return (
     <div>
-      <h1>Hola, estas en React</h1>
+      <h1>Hola {nombre}, estas en React</h1>
+      <h2>{nombre}</h2>
       <p>Este es mi primer JSX en React.</p>
       <p>Mi nombre es Alan</p>
+      <p>Mi edad en 5 años sera: {edad + 5}</p>
       <button>Presiona este boton</button>
+      <ul>
+        <li>{colores[0]}</li>
+        <li>{colores[1]}</li>
+        <li>{colores[2]}</li>
+        <li>{colores[3]}</li>
+      </ul>
       <ul>
         <li>Alma</li>
         <li>Irma</li>
