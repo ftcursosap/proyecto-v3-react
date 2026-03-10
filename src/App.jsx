17f6d53965ef55c9af860bc4c43443c8a5baa0e8
Alan <ftcursosap@gmail.com>
2026-03-10 17:39:17 -0500

    Semana 10 - dia 6

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,35 @@ import './App.css'
 
 function App() {
 
-  const nombre = "Alan";
-  
-  const frutas = ["Manzana", "Pera", "Mango", "Platano"];
+  const edad = 30;
+  const lenguajes = ["Javascript", "Python", "C#"];
 
-  return(
+  return (
+    
     <div>
-      <h1>Lista de frutas</h1>
+      <h1>Hola Alan</h1>
+      <p>Estoy aprendiendo React</p>
+      <p>Mi edad es: {edad}</p>
 
       <ul>
-        {frutas.map(function(fruta, index) {
-          return <li key={index}>{fruta}</li>;
+        <li>{lenguajes[0]}</li>
+        <li>{lenguajes[1]}</li>
+        <li>{lenguajes[2]}</li>
+      </ul>
+
+      <ul>
+        {lenguajes.map(function(lenguaje) {
+          return <li>{lenguaje}</li>
+        })}
+      </ul>
+
+      <ul>
+        {lenguajes.map(function(lenguaje, index) {
+          return <li key={index}>{lenguaje}</li>
         })}
       </ul>
     </div>
-  );
+  )
 }
 
 export default App
