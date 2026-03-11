e5c35f037e7365aefe9f9dcbd18be70dfe4b5bcf
Alan <ftcursosap@gmail.com>
2026-03-11 11:55:05 -0500

    Semana 11 - Dia 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [contador, setContador] = useState(10);
+  const [contador, setContador] = useState(2);
+
+  function aumentar() {
+    setContador(contador + 1);
+  }
+
+  function disminuir() {
+    setContador(contador - 1);
+  }
+
+  function cero() {
+    setContador(contador * 0);
+  }
 
   return(
     <div>
       <h1>Contador</h1>
-      <p>Contador Actual</p>
+      <h2>Mi contador en React</h2>
       <p>{contador}</p>
 
-      <button onClick={() => setContador(contador + 1)}>
-        Aumentar
-      </button>
-      <button onClick={() => setContador(contador - 1)}>
-        Disminuir
-      </button>
+      <button onClick={aumentar}>Suma 1</button>
+      <button onClick={disminuir}>Resta 1</button>
+      <button onClick={cero}>Valor cero</button>
+
     </div>
   );
   
